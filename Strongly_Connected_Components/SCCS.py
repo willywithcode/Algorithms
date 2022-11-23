6be1d34d5c7f131105049cf6db8476b42e4cb42a
WilliamNguyen <97029679+NullTreasure@users.noreply.github.com>
2022-11-23 11:34:34 +0700

    Update SCCS.py

--- Strongly_Connected_Components/SCCS.py
+++ Strongly_Connected_Components/SCCS.py
@@ -1,29 +1,26 @@
 
-# Python implementation of Kosaraju's algorithm to print all SCCs
-from sys import argv
-
 import sys
-sys.setrecursionlimit(300000)
+sys.setrecursionlimit(50000)
 from collections import defaultdict
 
 
 class Graph:
-	def __init__(self,vertices):
+	def __init__(self,vertices,Virtices):
 		self.V = vertices
 		self.graph = defaultdict(list)
+		self.Virtices = Virtices
 
 
 	def addEdges(self,u,v):
 		self.graph[u].append(v)
 
 
-	def DFSUntil(self, v, visited,size):
-		size +=1
+	def DFSUntil(self, v, visited):
 		visited[v] = True
 		for i in self.graph[v]:
 			if visited[i] == False:
-				return self.DFSUntil(i,visited,size)
-		return size
+				self.DFSUntil(i,visited)
+		
 		
 	
 
@@ -37,7 +34,7 @@ class Graph:
 
 
 	def Transpose(self):
-		gr = Graph(self.V)
+		gr = Graph(self.V,self.Virtices)
 		for i in self.graph:
 			for j in self.graph[i]:
 				gr.addEdges(j,i)
@@ -45,10 +42,10 @@ class Graph:
 	
 
 	def SCCs(self) :
+		sum = 0
 		stack = []
-		Size = []
 		visited = [False]*(self.V)
-		for i in range(self.V):
+		for i in self.Virtices:
 			if visited[i] ==False:
 				self.fillOrder(i,visited,stack)
 		
@@ -59,21 +56,17 @@ class Graph:
 		while stack:
 			i = stack.pop()
 			if visited[i] == False:
-				size = 0
-				temp =g.DFSUntil(i,visited,size)
-				Size.append(temp)
-
-		Size.sort(reverse= True)
-		for i in range(5):
-			print(Size[i])
-
-		print(Size)
+				g.DFSUntil(i,visited)
+				sum += 1
+		print(sum)
+		# Size.sort(reverse= True)
+		# for i in range(5):
+		# 	print(Size[i])
 
 
 
 
-
-
+Virtices = []
 
 with open("SCC.txt") as f:
 	read_list = f.readlines()
@@ -82,13 +75,16 @@ with open("SCC.txt") as f:
 V = len(read_list)
 for i in range(V):
 		read_list[i] = read_list[i].split()
-
-g = Graph(V)
 for i in range(V):
-	x = int (read_list[i][0])
-	y = int (read_list[i][1])
-	g.addEdges(x,y)
+	Virtices.append(int(read_list[i][0]))
+	Virtices.append(int(read_list[i][1]))
 
+g = Graph(len(set(Virtices)),set(Virtices))
+for i in range(V):
+	x = int(read_list[i][0])
+	y = int(read_list[i][1])
+	g.addEdges(x,y)
 g.SCCs()
+
 
 
